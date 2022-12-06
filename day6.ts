143b5a2b3ebd094948cb74fcd4385b7f95cb1899
PovilasG <pofeliuxxx@gmail.com>
2022-12-06 21:06:28 +0200

    Day 6 using reduce instead of while

--- day6.ts
+++ day6.ts
@@ -6,24 +6,19 @@ export const day6part2 = (datastream: string): number => {
 };
 
 const getMarkerPos = (datastream: string, markerSize: number): number => {
-  let differentCharsCount = 0;
-  let currentChars = '';
-  let remainingStream = datastream;
-  let i = 0;
-  while (remainingStream.length > 0 && differentCharsCount < markerSize) {
-    const currentChar = remainingStream[0];
-    if (currentChars.includes(currentChar)) {
-      const repeatedIndex = currentChars.indexOf(currentChar);
-      differentCharsCount = differentCharsCount - repeatedIndex;
+  let answer = 0;
+  datastream.split('').reduce((currentChars, char, currentIndex, arr) => {
+    if (currentChars.includes(char)) {
+      const repeatedIndex = currentChars.indexOf(char);
       currentChars = currentChars.substring(repeatedIndex + 1);
-    } else {
-      differentCharsCount = differentCharsCount + 1;
     }
-    currentChars = currentChars + currentChar;
-    remainingStream = remainingStream.substring(1);
-    i = i + 1;
-  }
-  return i;
+    if (currentChars.length + 1 === markerSize) {
+      answer = currentIndex + 1;
+      arr.splice(1);
+    }
+    return currentChars + char;
+  }, '');
+  return answer;
 };
 
 export default day6;
