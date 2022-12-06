438fe69252455d0ca96e56f4ecde07c10425cd9f
PovilasG <pofeliuxxx@gmail.com>
2022-12-06 20:44:37 +0200

    Day6 part 1

--- day6.test.ts
+++ day6.test.ts
@@ -8,5 +8,29 @@ describe('day6', () => {
         expect(day6(datastream)).toBe(7);
       });
     });
+    describe('bvwbjplbgvbhsrlpgdmjqwftvncz', () => {
+      const datastream = 'bvwbjplbgvbhsrlpgdmjqwftvncz';
+      it('should return 5', () => {
+        expect(day6(datastream)).toBe(5);
+      });
+    });
+    describe('nppdvjthqldpwncqszvftbrmjlhg', () => {
+      const datastream = 'nppdvjthqldpwncqszvftbrmjlhg';
+      it('should return 6', () => {
+        expect(day6(datastream)).toBe(6);
+      });
+    });
+    describe('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', () => {
+      const datastream = 'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg';
+      it('should return 10', () => {
+        expect(day6(datastream)).toBe(10);
+      });
+    });
+    describe('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', () => {
+      const datastream = 'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw';
+      it('should return 11', () => {
+        expect(day6(datastream)).toBe(11);
+      });
+    });
   });
 });
--- day6.ts
+++ day6.ts
@@ -1,7 +1,24 @@
-export const day6 = (input: string): number => {
-  return 7;
+export const day6 = (datastream: string): number => {
+  let differentCharsCount = 0;
+  let currentChars = '';
+  let remainingStream = datastream;
+  let i = 0;
+  while (remainingStream.length > 0 && differentCharsCount < 4) {
+    const currentChar = remainingStream[0];
+    if (currentChars.includes(currentChar)) {
+      const repeatedIndex = currentChars.indexOf(currentChar);
+      differentCharsCount = differentCharsCount - repeatedIndex;
+      currentChars = currentChars.substring(repeatedIndex + 1);
+    } else {
+      differentCharsCount = differentCharsCount + 1;
+    }
+    currentChars = currentChars + currentChar;
+    remainingStream = remainingStream.substring(1);
+    i = i + 1;
+  }
+  return i;
 };
-export const day6part2 = (input: string): number => {
+export const day6part2 = (datastream: string): number => {
   return 7;
 };
 
