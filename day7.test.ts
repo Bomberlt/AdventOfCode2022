cd35e0d98bf3e5ebf2390a7529581ec2103d087d
PovilasG <pofeliuxxx@gmail.com>
2022-12-07 22:24:45 +0200

    Day 7 parse one level

--- day7.test.ts
+++ day7.test.ts
@@ -1,4 +1,4 @@
-import day7 from './day7';
+import day7, { terminalInputToFilesystem } from './day7';
 
 describe('day7', () => {
   describe('terminal output is', () => {
@@ -50,6 +50,71 @@ $ ls
 7214296 k`;
       it('should return 95437', () => {
         expect(day7(terminalOutput)).toBe(95437);
+      });
+    });
+  });
+});
+
+describe('Terminal input to filesystem', () => {
+  describe('when terminal output is', () => {
+    describe(`$ cd /
+$ ls
+14848514 b.txt`, () => {
+      const terminalOutput = `$ cd /
+  $ ls
+  14848514 b.txt`;
+      const result = terminalInputToFilesystem(terminalOutput);
+      it('should return one folder', () => {
+        expect(result).toHaveLength(1);
+      });
+      it('should return folder name /', () => {
+        expect(result[0].name).toBe('/');
+      });
+      it('should return one file', () => {
+        expect(result[0].files).toHaveLength(1);
+      });
+    });
+  });
+  describe('when terminal output is', () => {
+    describe(`example`, () => {
+      const terminalOutput = `$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k`;
+      const result = terminalInputToFilesystem(terminalOutput);
+      it('should return one folder', () => {
+        expect(result).toHaveLength(1);
+      });
+      it('should return folder name /', () => {
+        expect(result[0].name).toBe('/');
+      });
+      describe('inside first folder', () => {
+        const firstFolder = result[0];
+        it('should have two folders', () => {
+          expect(firstFolder.folders).toHaveLength(2);
+        });
+        it('should have two files', () => {
+          expect(firstFolder.files).toHaveLength(2);
+        });
       });
     });
   });
--- day7.ts
+++ day7.ts
@@ -7,5 +7,60 @@ export const day7part2 = (
   return 95437;
 };
 
+export interface Folder {
+  folders: Array<Folder>;
+  files: Array<File>;
+  name: string;
+}
+
+export interface File {
+  name: string;
+  size: number;
+}
+
+export const terminalInputToFilesystem = (
+  terminalOutput: string
+): Array<Folder> => {
+  const terminalLines = terminalOutput
+    .replace(/\r/g, '')
+    .split(`\n`)
+    .map((line) => line.trimStart());
+  let name = '';
+  let folders = [];
+  let files = [];
+  terminalLines.every((line) => {
+    if (line.startsWith('$')) {
+      // Command
+      if (line.startsWith('$ cd')) {
+        if (line.split(' ').pop() !== '/') {
+          // TODO: Go deeper
+          return false;
+        }
+        // Open folder
+        name = line.split(' ').pop();
+      } else if (line.startsWith('$ ls')) {
+        // List contents
+      }
+    } else {
+      if (line.startsWith('dir')) {
+        const folderName = line.split(' ').pop();
+        folders.push({ name: folderName, folders: [], files: [] });
+      } else {
+        const fileName = line.split(' ').pop();
+        const fileSize = line.split(' ')[0];
+        files.push({ name: fileName, size: fileSize });
+      }
+    }
+    return true;
+  });
+  return [
+    {
+      folders: folders,
+      files: files,
+      name: name,
+    },
+  ];
+};
+
 export default day7;
 ``;
